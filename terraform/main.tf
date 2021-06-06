5007fd156e369c97cb3174a59ef227bc9d06a6ab
desema <desema.s7@gmail.com>
2021-06-06 18:31:02 +0300

    terraform basics

--- terraform/main.tf
+++ terraform/main.tf
@@ -72,3 +72,23 @@ resource "aws_instance" "test-amazon" {
     create_before_destroy = true
   }
 }
+
+module "ec2_cluster" {
+  source                 = "terraform-aws-modules/ec2-instance/aws"
+  version                = "~> 2.0"
+
+  name                   = "my-cluster"
+  instance_count         = 2
+
+  ami                    = "ami-0cf6f5c8a62fa5da6"
+  instance_type          = "t2.micro"
+  key_name               = "user1"
+  monitoring             = true
+  vpc_security_group_ids = ["sg-12345678"]
+  subnet_id              = "subnet-eddcdzz4"
+
+  tags = {
+    Terraform   = "true"
+    Environment = "dev"
+  }
+}
